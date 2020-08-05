df518181ab7f8079d263a0d8dc35e28078d0f2ed
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-08-05 12:28:33 +0100

    Changed the secret key in the settings.py file, now it is stored as an environment variable, meaning it isnt possible for others to find the secret key.

--- the_life_and_times/settings.py
+++ the_life_and_times/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yvcm8jr3r_3d$hjvp(l49#!!1jthlb1e1-+a11#z7m&4ys)@vv'
+SECRET_KEY = os.environ.get('SUPER_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,5 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
