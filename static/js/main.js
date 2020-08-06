88030dfac20dcf225e87dcfbc6bf244fa459f875
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-08-06 16:09:04 +0100

    The base.html page now has a navbar with a collapsible menu, it uses custom css change the appearance of the hamburger button, as well as using jquery to allow for the navbar to be hidden on scroll down, but reappear on scroll up. I decided to do this to provide a cleaner more minimalist aesthetic. I have also opted for a footer that is always present, as it contains the social links to my various other forms of connection.

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,31 @@
 $(function() {
     $('.card').matchHeight();
 });
+
+// jQuery allows for the navbar to collapse when clicking outside of the navbar,
+// rather than just on navbar-toggler button click
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+});
+
+// The jQuery allows for detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    let last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
+
