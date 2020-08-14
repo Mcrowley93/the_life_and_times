07d9e0f241cfc9e12f2980f8e65a3e7e637ffd27
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-08-14 12:42:14 +0100

    created the url pattern for the all_posts view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import post_list
+from blog.views import post_list, all_posts
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('all_posts/', all_posts, name='all_posts')
 ]
