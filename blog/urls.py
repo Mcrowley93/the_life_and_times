d1cca6ea5c46ff40c69da96558d85dcf6ea50935
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-08-21 13:46:35 +0100

    added urlpattern and view for the post_detail page. This will allow the user to click on an individual post and have it displayed on the screen

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views import post_list, all_posts
+from blog.views import post_list, all_posts, post_detail
 
 urlpatterns = [
     path('', post_list, name='post_list'),
-    path('all_posts/', all_posts, name='all_posts')
+    path('all_posts/', all_posts, name='all_posts'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 
@@ -13,3 +13,8 @@ def post_list(request):
 def all_posts(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/all_posts.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
