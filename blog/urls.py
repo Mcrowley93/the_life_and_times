cea0b9c05efa3b314a8e3d204e5dfc81c5a24e6a
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2020-10-09 11:06:13 +0100

    created url patterns for the blog app, including the home page (post_list) the all_posts page, which will display all posts that have been published to date, as well as the pages to create a new post, edit a post, and also to view an individual post in more detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import post_list, all_posts, post_detail
+from blog.views import post_list, all_posts, post_detail, new_post, post_edit
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('all_posts/', all_posts, name='all_posts'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', new_post, name='new_post'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
 ]
