9225a3dce6367fc1db78883178f864aef916fb1f
Martin Crowley <martin.k.s.crowley@hotmail.co.uk>
2021-01-24 14:17:16 +0000

    updated settings file to point towards the heroku app when the site is hosted

--- the_life_and_times/settings.py
+++ the_life_and_times/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SUPER_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('IP'), 'the-life-and-times.herokuapp.com']
 
 
 # Application definition
